2ea93cbf0fa0621844532b763c411a58cd02a48e
Smart Oghenemine <35765212+VinciDemons@users.noreply.github.com>
2024-03-20 20:50:15 +0100

    update

--- app/promotedcoins/page.js
+++ app/promotedcoins/page.js
@@ -1,13 +1,11 @@
 import PromotedCoin from "@/components/main/promoted-coin";
-import { fetchPromotedCoins, fetchVerifiedCoins } from "@/lib/fetchData";
 import React from "react";
 
 const PromotedCoinsPage = async () => {
-  const coinData = await fetchPromotedCoins();
-
+ 
   return (
     <>
-      <PromotedCoin coins={coinData} />
+      {/* <PromotedCoin coins={coinData} /> */}
     </>
   );
 };
