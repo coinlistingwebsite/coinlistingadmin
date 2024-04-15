4bfd3a46738033c4a6d1290333ed60ed9cea9464
Smart Oghenemine <35765212+VinciDemons@users.noreply.github.com>
2024-04-15 22:19:09 +0100

    fix

--- app/advertbanners/page.js
+++ app/advertbanners/page.js
@@ -10,12 +10,15 @@ import React from "react";
 export default async function AdvertBannersPage() {
   const banner = await fetchBanners();
 
-  if(!banner)  return (
-    <main className="max-w-7xl mx-auto py-40 text-center">
-      <h1 className="text-3xl">404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for could not be found.</p>
-    </main>
-  );
+  console.log(banner);
+
+  if (!banner)
+    return (
+      <main className="max-w-7xl mx-auto py-40 text-center">
+        <h1 className="text-3xl">404 - Page Not Found</h1>
+        <p>Sorry, the page you are looking for could not be found.</p>
+      </main>
+    );
 
   return (
     <div>
