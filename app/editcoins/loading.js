4e3d335e3b984c1b217bd4ebdad586b0e7a7f8d2
Smart Oghenemine <35765212+VinciDemons@users.noreply.github.com>
2024-04-23 20:50:29 +0100

    Addition of News & Delete Ended Presales

--- app/editcoins/loading.js
+++ app/editcoins/loading.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="w-full mx-auto">
+    <div className="w-full mx-auto ">
       <span className="loading loading-ball loading-xs"></span>
       <span className="loading loading-ball loading-sm"></span>
       <span className="loading loading-ball loading-md"></span>
