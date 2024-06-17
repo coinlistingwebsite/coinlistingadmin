665afc91dd16769df626fa423742ae15e86d46c5
Smart Oghenemine <35765212+VinciDemons@users.noreply.github.com>
2024-06-17 15:26:24 +0100

    update api

--- app/coin/[coinid]/page.js
+++ app/coin/[coinid]/page.js
@@ -4,6 +4,7 @@ import React from "react";
 
 export const dynamic = "force-dynamic";
 
+
 export default async function CoinEditPage({ params }) {
   const coinDetails = await fetchCoinById(params.coinid);
 
