634ad3f376fa1fcee9595388ba4bbbda8d87e44b
Smart Oghenemine <35765212+VinciDemons@users.noreply.github.com>
2024-06-17 15:35:11 +0100

    update

--- app/coin/[coinid]/page.js
+++ app/coin/[coinid]/page.js
@@ -3,8 +3,6 @@ import { fetchCoinById } from "@/lib/fetchData";
 import React from "react";
 
 export const dynamic = "force-dynamic";
-
-
 export default async function CoinEditPage({ params }) {
   const coinDetails = await fetchCoinById(params.coinid);
 
@@ -12,7 +10,7 @@ export default async function CoinEditPage({ params }) {
     return (
       <main className="max-w-7xl mx-auto py-40 text-center">
         <h1 className="text-3xl">404 - Page Not Found</h1>
-        <p>Sorry, the page you are looking for could not be found.</p>
+        <p>Sorry, the page you are looking for could not be found</p>
       </main>
     );
 
